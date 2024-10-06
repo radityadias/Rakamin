c6336cdccea6892056d83cdaf8875b4ab3ff236b
radityadias <radityadias24@gmail.com>
2024-10-06 13:54:26 +0700

    add close navbar-toggle function js

--- Homework CSS HTML/wisata.js
+++ Homework CSS HTML/wisata.js
@@ -8,5 +8,17 @@ window.addEventListener("scroll", () =>{
     }
 });
 
+document.addEventListener('click', function (event) {
+  const navbar = document.querySelector('.navbar-collapse');
+  const isNavbarOpen = navbar.classList.contains('show');
+  
+  // If the click is outside the navbar and the navbar is open, close it
+  if (isNavbarOpen && !navbar.contains(event.target) && !event.target.closest('.navbar-toggler')) {
+    document.querySelector('.navbar-toggler').click(); // Simulates closing the navbar
+  }
+});
+
+
+
 
 
